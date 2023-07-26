6e289c8bafdcb8768c06ab65eed4c842054e834c
shreyas0924 <shreyasp0924@gmail.com>
2023-07-26 23:19:30 +0530

    updated code or navbar

--- components/site-header.tsx
+++ components/site-header.tsx
@@ -1,28 +1,3 @@
-// import Link from "next/link"
-
-// import { siteConfig } from "@/config/site"
-// import { ThemeToggle } from "@/components/theme-toggle"
-
-// export function SiteHeader() {
-//   return (
-//     <header className="sticky top-0 z-40 w-full border-b bg-background">
-//       <div className="container flex h-16 items-center space-x-4 sm:justify-between sm:space-x-0">
-//         <Link href="/" className="flex gap-6 font-mono text-2xl md:gap-10">
-//           Shreyas P
-//         </Link>
-//         <div className="flex flex-1 items-center justify-end space-x-4">
-//           <nav className="flex items-center gap-4 space-x-1 font-mono text-2xl">
-//             <Link href="../projects">Projects</Link>
-//             <Link href={siteConfig.links.resume} target="_blank">
-//               Resume
-//             </Link>
-//             <ThemeToggle />
-//           </nav>
-//         </div>
-//       </div>
-//     </header>
-//   )
-// }
 "use client"
 
 import React from "react"
@@ -42,16 +17,16 @@ export function SiteHeader() {
   return (
     <header className="sticky top-0 z-40 w-full border-b bg-background">
       <div className="container flex h-16 items-center space-x-4 sm:justify-between sm:space-x-0">
-        <Link
-          href="/"
-          className="flex gap-6 font-mono text-xl md:text-2xl"
-        >
+        <Link href="/" className="flex gap-6 font-mono text-xl md:text-2xl">
           Shreyas P
         </Link>
         <div className="flex flex-1 items-center justify-end space-x-4">
           <nav className="flex items-center gap-4 space-x-1 font-mono text-xl md:text-2xl">
             <div className="hidden md:block">
-              <Link href="../projects" className="mx-4"> Projects</Link>
+              <Link href="../projects" className="mx-4">
+                {" "}
+                Projects
+              </Link>
               <Link href={siteConfig.links.resume} target="_blank">
                 Resume
               </Link>
